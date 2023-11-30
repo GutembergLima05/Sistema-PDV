f1d35c7f79785f67224ce4d2bde1472f438623fe
Gutemberg Lima <j.berglima05@gmail.com>
2023-11-29 22:48:00 -0300

    deploy: update

--- src/index.js
+++ src/index.js
@@ -6,3 +6,9 @@ const app = express();
 
 app.use(express.json())
 app.use(rotas)
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+})
