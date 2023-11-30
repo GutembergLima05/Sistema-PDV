8056003cd2f287e15319bacfabd79af0e23ad3cf
Victor Kawan <95768649+VictorKawan@users.noreply.github.com>
2023-11-30 15:16:25 -0300

    Update intermediarioTeste.js
    Signed-off-by: Victor Kawan <95768649+VictorKawan@users.noreply.github.com>

--- src/intermediarios/intermediarioTeste.js
+++ src/intermediarios/intermediarioTeste.js
@@ -1,9 +1,9 @@
-const testIntermediario = async (req, res, next) => {
+const intermediarioTeste = async (req, res, next) => {
     console.log("Passou no intermediario teste!");
 
     next();
 }
 
 module.exports = {
-    testIntermediario
+    intermediarioTeste
 }
