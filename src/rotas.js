c94268ebefe2a51319764a5c65719b832bab420f
Victor Kawan <95768649+VictorKawan@users.noreply.github.com>
2023-11-30 15:18:59 -0300

    Update rotas.js
    Signed-off-by: Victor Kawan <95768649+VictorKawan@users.noreply.github.com>

--- src/rotas.js
+++ src/rotas.js
@@ -9,17 +9,4 @@ const { cadastrarUser } = require('./controladores/usuario')
 // Rota pra cadastrar usuário
 rotas.post('/usuario', validador(usuario), cadastrarUser)
 
-
-// TESTES 
-
-// Importações de testes para serem removidas na daily amanhã.
-const { testeRoute } = require('../controladores/usuario');
-const intermediarioTeste = require('./intermediarios/intermediarioTeste');
-
-// Demonstração de intermediario na arquitetura do projeto...
-rotas.use(intermediarioTeste);
-
-// Demonstração de rota da arquitetura do projeto...
-rotas.get('/teste', testeRoute)
-
 module.exports = rotas
