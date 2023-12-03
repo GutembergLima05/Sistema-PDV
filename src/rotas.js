ac2143dd4544cc60177ed02541fefccaa60aed0c
Victor Kawan <95768649+VictorKawan@users.noreply.github.com>
2023-12-03 12:54:57 -0300

    Rotas "listar categorias" e "editar perfil" implementadas
    Signed-off-by: Victor Kawan <95768649+VictorKawan@users.noreply.github.com>

--- src/rotas.js
+++ src/rotas.js
@@ -13,4 +13,8 @@ rotas.post('/login', validador(esquemasUsuario.login), usuario.login)
 rotas.use(validarLogin)
 // Rota para detalhar usuario
 rotas.get('/usuario', usuario.detalharUsuario)
+// Rota para editar usuario
+rotas.put('/usuario', validador(esquemasUsuario.cadastroOuAtualizacao), usuario.editarUsuario)
+// Rota para listar categorias
+rotas.get('/categoria', usuario.listarCategoria)
 module.exports = rotas
