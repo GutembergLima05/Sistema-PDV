4a2d5499e4fdfbd6d00e427f3a48f54436e390f6
Gutemberg Lima <j.berglima05@gmail.com>
2023-12-05 20:55:26 -0300

    dump update

--- src/db/dump.sql
+++ src/db/dump.sql
@@ -23,6 +23,27 @@ insert into categorias (descricao) values
 ('Bebê'),
 ('Games');
 
+CREATE TABLE produtos (
+  id SERIAL PRIMARY KEY,
+  descricao TEXT NOT NULL,
+  quantidade_estoque INTEGER NOT NULL,
+  valor NUMERIC NOT NULL,
+  categoria_id INTEGER NOT NULL REFERENCES categorias(id)
+);
+
+
+CREATE TABLE clientes (
+  id SERIAL PRIMARY KEY,
+  nome TEXT NOT NULL,
+  email TEXT NOT NULL UNIQUE,
+  cpf TEXT NOT NULL UNIQUE,
+  cep TEXT,
+  rua TEXT,
+  numero TEXT,
+  bairro TEXT,
+  cidade TEXT,
+  estado TEXT
+);
 
 
 
