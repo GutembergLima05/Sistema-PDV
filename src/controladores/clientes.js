5b512d0809396338466203c96acb5bc4e23c7409
Edmilson dos Santos Júnior <matiasjr@live.com>
2023-12-07 18:01:11 -0300

    Criacao e finalizacao da funcao listar clientes

--- src/controladores/clientes.js
+++ src/controladores/clientes.js
@@ -18,4 +18,16 @@ const detalhar = async (req, res) => {
         return  res.status(500).json({ mensagem: 'Erro interno no servidor' });
     }
 }
-module.exports = {cadastrar, detalhar}
+
+const listarClientes = async (req, res) => {
+
+  try {
+    const clientes = await knex('clientes').orderBy('id', 'asc')
+
+    return res.status(200).json(clientes)
+    
+  } catch (error) {
+    return  res.status(500).json({ mensagem: 'Erro interno no servidor' });
+  }
+}
+module.exports = {cadastrar, detalhar, listarClientes}
