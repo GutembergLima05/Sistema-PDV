1e807661fad892a4c0f2afdaaa214cbf1f968804
Gutemberg Lima <j.berglima05@gmail.com>
2023-12-14 12:42:32 -0300

    bug: resolved system error

--- src/controladores/usuario.js
+++ src/controladores/usuario.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const senhaJwt = process.env.SENHA_JWT;
 
-const cadastrar = async (req, res) => {
+const cadastrarUsuario = async (req, res) => {
   const { nome, email, senha } = req.body;
 
   try {
@@ -21,7 +21,7 @@ const cadastrar = async (req, res) => {
   }
 };
 
-const login = async (req, res) => {
+const loginUsuario = async (req, res) => {
   const { email, senha } = req.body;
 
   try {
@@ -84,8 +84,8 @@ const listarCategoria = async (req, res) => {
   }
 };
 module.exports = {
-  login,
-  cadastrar,
+  loginUsuario,
+  cadastrarUsuario,
   detalharUsuario,
   editarUsuario,
   listarCategoria,
